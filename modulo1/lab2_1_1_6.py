888725533025f60d7195b74227674f68313b2424
ch1kilin <98619131+ch1kilin@users.noreply.github.com>
2023-01-25 17:55:18 -0600

    Add files via upload

--- modulo1/lab2_1_1_6.py
+++ modulo1/lab2_1_1_6.py
@@ -1,4 +1,9 @@
-#descripcion del programa #
+
+"""
+Nombre: angel de jesus rosas muñoz
+Fecha: 13/01/23
+Descripción: En este ejercicio solo es para imprimir en la consola 
+"""
 
 print("hola mundo!!")
 print("spy paquito")
